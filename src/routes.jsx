d029703f34386585b2da74e04cdadc0a5938f1bd
DivyaMuthina <divya.muthina.contractor@pepsico.com>
2024-12-06 13:24:16 +0530

    routes chnages

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,11 +5,11 @@ import { PageNotFound } from "./pages/unAuthenticated";
 export const router = () =>
   createBrowserRouter([
     {
-      path: "/",
+      path: "/My-First-React-App",
       element: <Home />,
     },
     {
-      path: "/about",
+      path: "/My-First-React-App/about",
       element: <About />,
     },
     {
